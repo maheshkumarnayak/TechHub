3b7829c3f184162352675c0ab7119eb03354385a
Mahesh Kumar Nayak <mahs.nayak@live.com>
2017-08-02 23:48:22 +0530

    Changed for visual studio change

--- Source/DotNetPOCHub/Angular2Mvc5Application1/app/utils/HttpHelpers.js
+++ Source/DotNetPOCHub/Angular2Mvc5Application1/app/utils/HttpHelpers.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var http_1 = require("@angular/http");
 var Observable_1 = require("rxjs/Observable");
 require("rxjs/Rx");
